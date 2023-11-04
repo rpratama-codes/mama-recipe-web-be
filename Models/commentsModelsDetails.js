c93b92e2c67373f2e7baecc40369afd3d079d69f
arsyad12 <33723389+arsyad12@users.noreply.github.com>
2023-11-04 19:29:37 +0700

    update model coment

--- Models/commentsModelsDetails.js
+++ Models/commentsModelsDetails.js
@@ -15,7 +15,12 @@ const commentsModelsDetails = {
   },
 
   getCommentByUID: async (recipeUid) => {
-    const request = await database`SELECT recipe_uid,user_uid,message FROM comments WHERE recipe_uid = ${recipeUid}`
+    const request = await database`select
+    CONCAT(u.first_name,' ',u.last_name) as name,
+    message, photo_profile as photo
+    from "comments" c  left join users u
+    on c.user_uid = u.user_uid
+    where recipe_uid = ${recipeUid}`
     return request
   }
 }
