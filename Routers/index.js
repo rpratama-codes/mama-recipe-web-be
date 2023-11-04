4171ecb0a280da3fbcccc9043c328bcc84dc694b
Rizqi Pratama <35159027+rizqikazukun@users.noreply.github.com>
2023-11-04 15:32:23 +0700

    [FIX] adjust some endpoint

--- Routers/index.js
+++ Routers/index.js
@@ -24,15 +24,13 @@ router.get('/recipes/:receiptUid', receipesControllerDetails._getRecipesByParams
 // comment endpoint
 router.get('/comments', commentsControllerDetails._getAllComments)
 router.post('/comments', commentsControllerDetails._addComment)
-router.get('/recipes/:title/detail', receipesControllerDetails._getRecipesByTitle)
+router.post('/recipes/:title/detail', receipesControllerDetails._getRecipesByTitle)
 router.get('/recipes/:recipeUid/detail/comments', commentsControllerDetails._getCommentByUID)
 
 // recipe Popular endpoint
 router.get('/home/popular', receipeHomeController.getPopulareRecipe_Controller)
 router.get('/home/list', receipeHomeController.getListRecipe_Controller)
 router.get('/home/new', receipeHomeController.getNewRecipe_Controller)
-router.get('/recipes/popular/:y', receipeHomeController.getPopulareRecipe_Controller)
-router.get('/recipes/popular', receipeHomeController.getListRecipe_Controller)
 
 // user endpoint
 router.post('/user/register', userControllers._userRegister)
