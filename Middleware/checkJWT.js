c7f4b68c090c9f7d37f23960ee5525f2a9ed97ba
Rizqi Pratama <35159027+rizqikazukun@users.noreply.github.com>
2023-11-26 20:11:48 +0700

    🔷 update error response status

--- Middleware/checkJWT.js
+++ Middleware/checkJWT.js
@@ -1,20 +1,20 @@
 const jwt = require('jsonwebtoken')
 
 const chekJWT = {
-
   jwtTokenDecode: async (req, res, next) => {
     try {
       const token = req.headers.authorization.slice(7)
-      console.log(token)
       const decoded = jwt.verify(token, process.env.APP_SECRET_TOKEN)
-      console.log(decoded)
-      if (decoded) { // nah kalo sesuai maka kita bisa lanjut masuk ke bagian controller dengan parameter next
+
+      if (decoded) {
         next()
+      } else {
+        throw { success: false, message: 'unauthorize' }
       }
     } catch (error) {
-      res.status(304).json({
-        status: false,
-        message: console.log(error)
+      res.status(401).json({
+        success: false,
+        message: 'unautorize'
       })
     }
   }
