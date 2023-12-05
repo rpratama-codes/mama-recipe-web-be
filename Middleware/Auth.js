1bc1df77a669336796092ba707fdb6db87b7a20b
Rizqi Pratama <35159027+rizqikazukun@users.noreply.github.com>
2023-12-05 23:26:59 +0700

    🔷 update update auth

--- Middleware/Auth.js
+++ Middleware/Auth.js
@@ -3,8 +3,7 @@ const jwt = require('jsonwebtoken')
 const Auth = {
   verify: async (req, res, next) => {
     try {
-      const token = req.headers.authorization.slice(7)
-
+      const token = req?.headers?.authorization?.slice(7)
       const decoded = jwt.verify(token, process.env.APP_SECRET_TOKEN)
 
       if (decoded) {
@@ -14,14 +13,19 @@ const Auth = {
         throw { success: false, status: 401, message: 'unauthorize' }
       }
     } catch (error) {
-      if (error.status === 401) {
+      let customMessage = ''
+      customMessage += 'please not directy consume this message to the client, '
+      customMessage += "you're try to access the endpoint that need a login, "
+      customMessage += 'or you has not correctly attach the auth headers.'
+
+      if (error.status === 401 || error.message.includes('must be provided')) {
         res.status(401).json({
-          success: 401,
-          message: error.message
+          status: 401,
+          message: customMessage
         })
       } else {
         res.status(500).json({
-          success: 500,
+          status: 500,
           message: error.message
         })
       }
