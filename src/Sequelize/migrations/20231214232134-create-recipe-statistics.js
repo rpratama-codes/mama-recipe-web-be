f2ae9a100ab4296b96deebac9fc2c54056f09355
Rizqi Pratama <35159027+rizqikazukun@users.noreply.github.com>
2023-12-15 06:32:29 +0700

    ✅ adding table & migration statistics

---
+++ src/Sequelize/migrations/20231214232134-create-recipe-statistics.js
@@ -0,0 +1,37 @@
+'use strict'
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('recipe_statistics', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      recipes_uid: {
+        type: Sequelize.UUID
+      },
+      views: {
+        type: Sequelize.FLOAT
+      },
+      likes: {
+        type: Sequelize.FLOAT
+      },
+      bookmarked: {
+        type: Sequelize.FLOAT
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    })
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('recipe_statistics')
+  }
+}
